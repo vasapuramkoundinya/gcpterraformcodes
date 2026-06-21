eb0b1edf3d0dd555cdc57cf55683cf59732bd8d0
narasimha.vasapuram@gmail.com <narasimha.vasapuram@gmail.com>
2026-06-21 15:11:17 +0530

    update jenkins file

--- gcporganizations/composition/us-central1/dev/project-neuralspun/main.tf
+++ gcporganizations/composition/us-central1/dev/project-neuralspun/main.tf
@@ -3,5 +3,5 @@ resource "google_project" "my_project" {
   project_id = var.project_id
   #org_id     = "1234567"
   tags = {"env":"staging"}
-  billing_account = var.billing_account
+  #billing_account = var.billing_account
 }
