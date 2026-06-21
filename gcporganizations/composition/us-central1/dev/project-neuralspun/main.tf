f35c69092f458c0ee7c7d36c53290b178604cc71
narasimha.vasapuram@gmail.com <narasimha.vasapuram@gmail.com>
2026-06-21 15:33:23 +0530

    update jenkins file

--- gcporganizations/composition/us-central1/dev/project-neuralspun/main.tf
+++ gcporganizations/composition/us-central1/dev/project-neuralspun/main.tf
@@ -1,13 +1,14 @@
-resource "google_project" "my_project" {
+/*resource "google_project" "my_project" {
   name       = var.name
   project_id = var.project_id
   #org_id     = "1234567"
   tags = {"env":"staging"}
   #billing_account = var.billing_account
-}
+}*/
 
 
 resource "google_service_account" "service_account" {
   account_id   = "sa-newtf"
   display_name = "Service Account for terraform new SA"
+  #project_id = var.project_id
 }
--- gcporganizations/composition/us-central1/dev/project-neuralspun/terraform.tfvars
+++ gcporganizations/composition/us-central1/dev/project-neuralspun/terraform.tfvars
@@ -1,4 +1,4 @@
-project_id ="secondproject"
-name = "secondproject"
-billing_account = "01A84A-B24D8F-D23C33"
+project_id ="neural-sunup-416408"
+#name = "secondproject"
+#billing_account = "01A84A-B24D8F-D23C33"
 region="us-central1"
